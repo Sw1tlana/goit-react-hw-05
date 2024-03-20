5141504d73d32eabd7ef054dfb75254dc273d62b
Sw1tlana <127736251+Sw1tlana@users.noreply.github.com>
2024-03-20 22:09:30 +0200

    Changes MovieDetalisPage

--- src/pages/MovieDetailsPage.jsx
+++ src/pages/MovieDetailsPage.jsx
@@ -1,34 +1,37 @@
-import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom"
-import { requestMoviesById } from "../services/api"
-import Loader from '../components/Loader/Loader';
-import ErrorMessage from '../components/ErrorMessage/ErrorMessage';
+// import { useEffect, useState } from "react";
+// import { useParams } from "react-router-dom"
+// import { requestMoviesById } from "../services/api"
+// import Loader from '../components/Loader/Loader';
+// import ErrorMessage from '../components/ErrorMessage/ErrorMessage';
 
-const MovieDetailsPage = () => {
-    const { movieId } = useParams();
-    const [moviId, setMoviId] = useState(null);
-    const [isLoading, setIsLoading] = useState(false);
-    const [isError, setIsError] = useState(false);
+// const MovieDetailsPage = () => {
+//     const { movieId } = useParams();
+//     const [moviId, setMoviId] = useState(null);
+//     const [isLoading, setIsLoading] = useState(false);
+//     const [isError, setIsError] = useState(false);
 
-    useEffect(() => {
-        const fetchMoviesId = async () => {
-            try {
-                setIsLoading(true);
-                setIsError(false);
-                const data = requestMoviesById(moviId);
-                setMoviId(data);
-            } catch (error) {
-                setIsError(true);
-            } finally {
-                setIsLoading(false);
-            }
-        }
-        fetchMoviesId();
-    }, [moviId]);
+//     useEffect(() => {
+//         const fetchMoviesId = async () => {
+//             try {
+//                 setIsLoading(true);
+//                 setIsError(false);
+//                 const data = requestMoviesById(moviId);
+//                 setMoviId(data);
+//             } catch (error) {
+//                 setIsError(true);
+//             } finally {
+//                 setIsLoading(false);
+//             }
+//         }
+//         fetchMoviesId();
+//     }, [moviId]);
 
-  return (
-      <div>{ movieId }</div>
-  )
-}
+//   return (
+//       <div>
+//           {isLoading && <Loader />}
+//           {isError && <ErrorMessage />}
+//       </div>
+//   )
+// }
 
-export default MovieDetailsPage
+// export default MovieDetailsPage
